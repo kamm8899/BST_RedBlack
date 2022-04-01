699d230e9a0d8a7035d5159e65a93fc2e87dab40
kamm8899 <kamm8899@gmail.com>
2022-04-01 12:40:18 -0700

    final product for the algorithm

--- main.cpp
+++ main.cpp
@@ -144,9 +144,9 @@ int main(int argc, char* argv[])
 	//rb.inorder_output();
 
 
-	// for (int i = 0; (i < n) && (i < 20); i++)
-	// 	cout << input_array[i] << endl;
-	// cout << endl;
+//	 for (int i = 0; (i < n) && (i < 20); i++)
+//	 	cout << input_array[i] << endl;
+//	 cout << endl;
 
 /*
  * free allocated storage
